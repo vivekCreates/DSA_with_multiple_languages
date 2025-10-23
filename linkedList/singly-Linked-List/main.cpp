2d85f7ccf3997d385920fcb0c58ed0d37b5761fa
Vivek <kumarvivek83561@gmail.com>
2025-10-23 20:54:26 +0530

    Refactor code structure for improved readability and maintainability

---
+++ linkedList/singly-Linked-List/main.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List
+{
+    Node *head;
+    Node *tail;
+public:
+    List(){
+        head = tail = NULL;
+    }
+
+    void pushFront(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = tail = newNode;
+            return;
+        }
+        else
+        {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    void pushBack(int val)
+    {
+        Node* newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = tail = newNode;
+            return;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void popFront()
+    {
+        if (head == NULL)
+        {
+            cout << "List is empty, You can't pop";
+            return;
+        }
+        Node *temp = head;
+        head = temp->next;
+    }
+
+    void popBack()
+    {
+        if (head == NULL)
+        {
+            cout << "List is empty, You can't pop";
+            return;
+        }
+        Node *temp = head;
+
+        while (temp->next->next)
+        {
+            temp = temp->next;
+        }
+        temp->next = NULL;
+        tail = temp;
+    }
+
+    void print(){
+        Node* temp = head;
+        while (temp != NULL){
+            cout << temp->data << " -->";
+            temp = temp->next;
+        }
+    }
+};
+
+int main()
+{
+    List list;
+
+    list.pushFront(10);
+    list.pushBack(20);
+    list.print();
+
+    return 0;
+}
