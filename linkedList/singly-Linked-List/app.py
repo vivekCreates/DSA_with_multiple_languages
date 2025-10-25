d9859e836bcbd251738c24d7715e3c952b9bc513
Vivek <kumarvivek83561@gmail.com>
2025-10-25 15:59:06 +0530

    Implement insert_at method and update size function in singly linked list

--- linkedList/singly-Linked-List/app.py
+++ linkedList/singly-Linked-List/app.py
@@ -38,20 +38,40 @@ class List:
         self.tail = temp
         self.tail.next = None
       
+    def insert_at(self,pos,val):
+        newNode = Node(val)
+        temp = self.head
+        for i in range(1,pos-1):
+            temp = temp.next
+        newNode.next = temp.next
+        temp.next = newNode
+        
     def print_list(self):
         temp = self.head 
         while temp:
             print(temp.data)
             temp = temp.next 
          
+    def size(self):
+        temp = self.head
+        element = 0 
+        while(temp):
+            element+=1
+            temp = temp.next
+        return element  
     
-    
+    def clear(self):
+        self.head = self.tail = None
+  
 l1 = List()
+
 
 l1.push_front(1)
 l1.push_front(2)
 l1.push_back(3)
 l1.pop_front()
 l1.pop_back()
-
+l1.insert_at(2,100)
 l1.print_list()
+
+print(l1.size())
